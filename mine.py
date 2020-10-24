19e2681d77dad59cc30cfe6124d4b944acb3ce5f
Xiaoqin Yan <yanxiaoq@msu.edu>
2020-10-24 15:49:47 -0400

    Update mine.py

--- mine.py
+++ mine.py
@@ -4,3 +4,11 @@ import spacy
 
 from constants
 import MINED_FOLDER, LOG_FORMAT
+
+def is_useful(doc, minlen=9, maxlen=18):
+	"""Condition Function."""
+	min_c = len(doc) >= minlen
+	max_c = len(doc) <= maxlen
+	upper_c = doc[0].text[0].isupper()
+	end_c = doc.text.endswith(':\n') or doc.text.endswith(': \n')
+	return min_c and max_c and upper_c and not end_c
