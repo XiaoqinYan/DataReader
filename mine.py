9408bb758a3eafe6ce7b515fa3e8adb1e2a79752
Xiaoqin Yan <yanxiaoq@msu.edu>
2020-10-24 15:50:18 -0400

    Update mine.py

--- mine.py
+++ mine.py
@@ -12,3 +12,9 @@ def is_useful(doc, minlen=9, maxlen=18):
 	upper_c = doc[0].text[0].isupper()
 	end_c = doc.text.endswith(':\n') or doc.text.endswith(': \n')
 	return min_c and max_c and upper_c and not end_c
+
+def already_mined(folder_name):
+	"""If mining process fails, this function will restart it."""
+	file_name = folder_name + '.txt'
+	return file_name in os.listdir(MINED_FOLDER) 
+
